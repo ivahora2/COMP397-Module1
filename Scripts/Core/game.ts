146a1f049d7a509429074a2e921464bcab384ec4
ivahora2 <ivahora2@my.centennialcollege.ca>
2018-07-03 16:15:16 -0400

    Rottion On label

--- Scripts/Core/game.ts
+++ Scripts/Core/game.ts
@@ -1,32 +1,41 @@
 //IIFE - Immediate;y invoked function expression
-(function () {
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    function Init() {
+(function(){
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+
+
+    function Init():void{
         console.log("Intialization Started");
-        var myVariable = "foo";
-        Start();
+
+        let myVariable: string = "foo";
+         Start();
+
     }
-    function Start() {
+
+    function Start():void{
         console.log("Starting pllication");
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60;
-        createjs.Ticker.on("tick", Update);
+        createjs.Ticker.on("tick",Update);
         Main();
     }
-    function Update() {
-        helloLabel.rotation += 5;
+
+    function Update():void{
+        helloLabel.rotation +=5;
         stage.update();
     }
-    function Main() {
+
+    function Main():void{
         console.log("Game Started");
-        helloLabel = new createjs.Text("Hello World", "40px Elephant", "red");
+        helloLabel = new createjs.Text("Hello World","40px Elephant","red");
         console.log(helloLabel);
         helloLabel.x = 100;
         helloLabel.y = 200;
         stage.addChild(helloLabel);
     }
+
     window.onload = Init;
+
 })();
-//# sourceMappingURL=game.js.map
+
