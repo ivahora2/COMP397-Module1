2cb22f137e9ed097932af351aeeb8b5375c314de
ivahora2 <ivahora2@my.centennialcollege.ca>
2018-07-03 12:21:06 -0400

    Inital Commit

---
+++ Scripts/Core/game.js
@@ -0,0 +1,23 @@
+//IIFE - Immediate;y invoked function expression
+(function(){
+
+    function Init(){
+        console.log("Intialization Started");
+    }
+
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    function Main(){
+
+    }
+
+    window.onload = Init;
+
+})();
+
